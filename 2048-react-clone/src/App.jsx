d6768914b4582d8e374bcb7c5cfde965e7c2538a
Hugh Stapleton <hstapleton808@gmail.com>
2024-03-19 17:30:56 -0400

    working move right logic

--- 2048-react-clone/src/App.jsx
+++ 2048-react-clone/src/App.jsx
@@ -4,13 +4,82 @@ import Scoreboard from './Components/Scoreboard.jsx';
 import Board from './Components/Board.jsx';
 
 function App() {
-  const boardDefault = Array(4).fill(Array(4).fill(null));
-  const boardTest = [[2,null,null,null],
+  // const boardDefault = Array(4).fill(Array(4).fill(null));
+  const boardTest = 
+  [[null,2,null,null],
   [null,null,null,null],
   [null,null,null,null],
   [null,null,null,null], ]
 
   const [board, setBoard] = useState(boardTest);
+
+  function moveRight() {
+    //loop over board
+    const newBoard = [...board]
+    for (let row = 0; row < 4; row++) {
+      let index1 = 3;
+      let index2 = 2;
+      let nullPt = undefined;
+
+      while (newBoard[row][index2] !== undefined) {
+
+        if (newBoard[row][index1] === newBoard[row][index2] && newBoard[row][index1] !== null) {
+          if (nullPt !== undefined) {
+            newBoard[row][nullPt] = newBoard[row][index1] * 2;
+            newBoard[row][index2] = null;
+            newBoard[row][index1] = null;
+            nullPt = index1;
+          }
+          else {
+            newBoard[row][index1] = newBoard[row][index1] * 2;
+            newBoard[row][index2] = null;
+          }
+          index1--;
+          index2--;
+          continue;
+        }
+        if (newBoard[row][index2] === null) {
+          index2--;
+          continue;
+        }
+        if (newBoard[row][index1] === null) {
+          if (nullPt === undefined) nullPt = index1;
+          if (index2 - 1 < 0) {
+            newBoard[row][nullPt] = newBoard[row][index2]
+            newBoard[row][index2] = null;
+          }
+          index1--;
+          index2--;
+          continue;
+        }
+      }
+    }
+    setBoard(newBoard);
+  }
+
+  document.addEventListener("keydown", e => {
+    if (e.key === 'ArrowUp') {
+      e.preventDefault();
+      console.log('up!')
+      // moveUp();
+    }
+    else if (e.key === 'ArrowLeft') {
+      e.preventDefault();
+      console.log('left!')
+    }
+    else if (e.key === 'ArrowRight') {
+      e.preventDefault();
+      console.log('right!')
+      moveRight();
+    }
+    else if (e.key === 'ArrowDown') {
+      e.preventDefault();
+      console.log('down!')
+    }
+  })
+  
+  //move right
+  //until element is undefined, or a number
 
   
   return (
