853a347b95ef02c1d5388085494c609c32dafd09
Hugh Stapleton <hstapleton808@gmail.com>
2024-03-20 11:31:08 -0400

    updated move functions

--- 2048-react-clone/src/App.jsx
+++ 2048-react-clone/src/App.jsx
@@ -6,76 +6,111 @@ import Board from './Components/Board.jsx';
 function App() {
   // const boardDefault = Array(4).fill(Array(4).fill(null));
   const boardTest = 
-  [[null,2,null,null],
-  [2,2,2,2],
-  [null,null,2,2],
-  [2,null,2,2], ]
+  [[2,null,null,null],
+  [2,null,null,null],
+  [null,null,null,null],
+  [null,null,null,null], ]
 
   const [board, setBoard] = useState(boardTest);
 
   function moveRight() {
-    //loop over board
+
     const newBoard = [...board]
+
     for (let row = 0; row < 4; row++) {
-      let index1 = 3;
-      let index2 = 2;
-      let nullPt = undefined;
+      let result = newBoard[row].filter( (el) => el !== null);
+      let i = result.length - 1;
 
-      while (newBoard[row][index2] !== undefined) {
-
-        if (newBoard[row][index1] === newBoard[row][index2] && newBoard[row][index1] !== null) {
-          if (nullPt !== undefined) {
-            newBoard[row][nullPt] = newBoard[row][index1] * 2;
-            newBoard[row][index2] = null;
-            newBoard[row][index1] = null;
-            nullPt = index1;
-          }
-          else {
-            newBoard[row][index1] = newBoard[row][index1] * 2;
-            newBoard[row][index2] = null;
-          }
-          index1--;
-          index2--;
-          continue;
-        }
-        if (newBoard[row][index2] === null) {
-          index2--;
-          continue;
-        }
-        if (newBoard[row][index1] === null ) {
-          if (nullPt === undefined) nullPt = index1;
-          if (index2 - 1 < 0) {
-            newBoard[row][nullPt] = newBoard[row][index2]
-            newBoard[row][index2] = null;
-          }
-          index1--;
-          index2--;
-          continue;
-        }
-        if (newBoard[row][index1] && nullPt !== undefined) {
-          newBoard[row][nullPt] = newBoard[row][index1]
-          newBoard[row][index1] = null;
-          index1--;
+      while (i > 0) {
+        if (result[i] === result[i - 1]) {
+          result[i] *= 2;
+          result[i - 1] = null;
+          i -= 2;
+        } else {
+          i--;
         }
       }
+      
+      result = result.filter( (el) => el !== null);
+      
+      while (result.length < 4) {
+        result.unshift(null);
+      }
+
+      newBoard[row] = result;
     }
-    setBoard(newBoard);
+    return newBoard;
   }
+  
+  function moveLeft() {
+
+    const newBoard = [...board]
+
+    for (let row = 0; row < 4; row++) {
+      let result = newBoard[row].filter( (el) => el !== null);
+      let i = 0;
+
+      while (i < result.length - 1) {
+        if (result[i] === result[i + 1]) {
+          console.log('Made it to while loop', row)
+          result[i] *= 2;
+          result[i + 1] = null;
+          i += 2;
+        } else {
+          i++;
+        }
+      }
+      
+      result = result.filter( (el) => el !== null);
+      
+      while (result.length < 4) {
+        result.push(null);
+      }
+
+      newBoard[row] = result;
+    }
+    return newBoard;
+  }
+
+  function moveUp() {
+    const flippedBoard = Array(4).fill().map(() => Array(4).fill(null));
+    console.log('flippedBoard ', flippedBoard)
+    board.forEach((row, rowIndex) => {
+      row.forEach((tile, colIndex) => {
+        flippedBoard[colIndex][rowIndex] = tile;
+      })
+    })
+   const newBoard = moveLeft();
+    
+   const unFlippedBoard = Array(4).fill().map(() => Array(4).fill(null));
+    newBoard.forEach((row, rowIndex) => {
+      row.forEach((tile, colIndex) => {
+        unFlippedBoard[rowIndex][colIndex] = tile;
+      })
+    })
+    return unFlippedBoard;
+  }
+
+  
 
   document.addEventListener("keydown", e => {
     if (e.key === 'ArrowUp') {
       e.preventDefault();
       console.log('up!')
-      // moveUp();
+      const newBoard = moveUp();
+      setBoard(newBoard);
     }
     else if (e.key === 'ArrowLeft') {
       e.preventDefault();
-      console.log('left!')
+      console.log('left!');
+      const newBoard = moveLeft();
+      setBoard(newBoard);
     }
     else if (e.key === 'ArrowRight') {
       e.preventDefault();
       console.log('right!')
-      moveRight();
+      const newBoard = moveRight();
+      setBoard(newBoard);
     }
     else if (e.key === 'ArrowDown') {
       e.preventDefault();
