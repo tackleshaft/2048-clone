bdb1caac895a6ab6285fe1a03063053ca2caaac0
Hugh Stapleton <hstapleton808@gmail.com>
2024-03-22 10:56:00 -0400

    fully working arrow keys

--- 2048-react-clone/src/App.jsx
+++ 2048-react-clone/src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css';
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useLayoutEffect } from 'react';
 import Scoreboard from './Components/Scoreboard.jsx';
 import Board from './Components/Board.jsx';
 
@@ -9,18 +9,43 @@ function App() {
   [[2,null,null,null],
   [2,null,2,null],
   [null,null,2,null],
-  [8,4,null,4], ]
+  [4,8,8,8], ]
 
   const [board, setBoard] = useState(boardTest);
 
+  const rotateLeft = function (matrix) {
+    const rows = matrix.length;
+    const columns = matrix[0].length;
+    const res = [];
+    for (let row = 0; row < rows; ++row) {
+      res.push([]);
+      for (let column = 0; column < columns; ++column) {
+        res[row][column] = matrix[column][columns - row - 1];
+      }
+    }
+    return res;
+  };
+
+  const rotateRight = function(matrix) {
+    const rows = matrix.length;
+    const columns = matrix[0].length;
+    const res = [];
+    for (let row = 0; row < rows; ++row) {
+        res.push([]);
+        for (let column = 0; column < columns; ++column) {
+            res[row][column] = matrix[rows - column - 1][row];
+        }
+    }
+    return res;
+};
+
   function moveRight() {
 
-    const newBoard = [...board]
+    const newBoard = board.map((array) => [...array]);
 
     for (let row = 0; row < 4; row++) {
       let result = newBoard[row].filter( (el) => el !== null);
       let i = result.length - 1;
-
       while (i > 0) {
         if (result[i] === result[i - 1]) {
           result[i] *= 2;
@@ -30,21 +55,19 @@ function App() {
           i--;
         }
       }
-      
       result = result.filter( (el) => el !== null);
       
       while (result.length < 4) {
         result.unshift(null);
       }
-
       newBoard[row] = result;
     }
-    return newBoard;
+    setBoard(newBoard);
   }
   
   function moveLeft() {
 
-    const newBoard = [...board]
+    const newBoard = board.map((array) => [...array]);
 
     for (let row = 0; row < 4; row++) {
       let result = newBoard[row].filter( (el) => el !== null);
@@ -68,19 +91,17 @@ function App() {
 
       newBoard[row] = result;
     }
-    return newBoard;
+    setBoard(newBoard);
   }
 
   function moveUp() {
-    const flippedBoard = Array(4).fill().map(() => Array(4).fill(null));
-    board.forEach((row, rowIndex) => {
-      row.forEach((tile, colIndex) => {
-        flippedBoard[colIndex][rowIndex] = tile;
-      })
-    })
 
+    let newBoard = board.map((array) => [...array]);
+
+    const rotatedBoard = rotateLeft(newBoard);
+    
     for (let row = 0; row < 4; row++) {
-      let result = flippedBoard[row].filter( (el) => el !== null);
+      let result = rotatedBoard[row].filter( (el) => el !== null);
       let i = 0;
 
       while (i < result.length - 1) {
@@ -99,50 +120,76 @@ function App() {
         result.push(null);
       }
 
-      flippedBoard[row] = result;
+      rotatedBoard[row] = result;
     }
-    
-    const unFlippedBoard = Array(4).fill().map(() => Array(4).fill(null));
-    flippedBoard.forEach((row, rowIndex) => {
-      row.forEach((tile, colIndex) => {
-        unFlippedBoard[colIndex][rowIndex] = tile;
-      })
-    })
-    
-    return unFlippedBoard;
+
+    newBoard = rotateRight(rotatedBoard)
+    setBoard(newBoard);
   }
 
+  function moveDown() {
+    
+    let newBoard = board.map((array) => [...array]);
+
+    const rotatedBoard = rotateRight(newBoard);
+    
+    for (let row = 0; row < 4; row++) {
+      let result = rotatedBoard[row].filter( (el) => el !== null);
+      let i = 0;
+
+      while (i < result.length - 1) {
+        if (result[i] === result[i + 1]) {
+          result[i] *= 2;
+          result[i + 1] = null;
+          i += 2;
+        } else {
+          i++;
+        }
+      }
+      
+      result = result.filter( (el) => el !== null);
+      
+      while (result.length < 4) {
+        result.push(null);
+      }
+
+      rotatedBoard[row] = result;
+    }
+
+    newBoard = rotateLeft(rotatedBoard)
+    setBoard(newBoard);
+
+  }
+
+  useEffect(() => console.log('board[3]: ', board[3]), [board]);
   
   useEffect(() => {
-    const handleKeyDown = (e) => {
-      e.preventDefault(); // Prevent default behavior
-      
-      if (e.key === 'ArrowUp') {
-        console.log('up!')
-        const newBoard = moveUp();
-        setBoard(newBoard);
-      }
-      else if (e.key === 'ArrowLeft') {
-        console.log('left!');
-        const newBoard = moveLeft();
-        setBoard(newBoard);
-      }
-      else if (e.key === 'ArrowRight') {
-        console.log('right!')
-        const newBoard = moveRight();
-        setBoard(newBoard);
-      }
-      else if (e.key === 'ArrowDown') {
-        console.log('down!')
-      }
-    };
-  
+  const handleKeyDown = (e) => {
+    e.preventDefault(); // Prevent default behavior
+    
+    if (e.key === 'ArrowUp') {
+      console.log('up!')
+      moveUp();
+    }
+    else if (e.key === 'ArrowLeft') {
+      console.log('left!');
+      moveLeft();
+    }
+    else if (e.key === 'ArrowRight') {
+      console.log('right!')
+      moveRight();
+    }
+    else if (e.key === 'ArrowDown') {
+      console.log('down!');
+      moveDown();
+    }
+  };
     document.addEventListener("keydown", handleKeyDown);
   
     return () => {
       document.removeEventListener("keydown", handleKeyDown);
     };
-  }, []);
+  }, [board]);
 
   
   return (
--- 2048-react-clone/src/Components/Tile.jsx
+++ 2048-react-clone/src/Components/Tile.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 function Tile({ row, col, tileVal }) {
+  
   return (
     <div className="tile">{tileVal}</div>
   )
